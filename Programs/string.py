c7f8ccde1b2efaa16e07d9a59b1f0ce59f0a8e26
rastr <rastradhoj@gmail.com>
2021-02-24 11:56:03 +0545

    All files

---
+++ Programs/string.py
@@ -0,0 +1,7 @@
+#WAP using different string manipulation.
+name = input("Enter your name:")
+age = int(input("Enter age:"))
+print("Hello my name is " + name + " and I am " + str(age) + " years old")
+print("Hello my name is", name, "and I am", age, "years old")
+print("Hello my name is %s and I am %d years old" % (name, age))
+print("Hell0 my name is {} and I am {} years old".format(name, age))
