76859dee22149d1b0fd124311bc05425b7fd102d
rastr <rastradhoj@gmail.com>
2021-02-24 11:45:50 +0545

    All files

---
+++ Programs/name&age.py
@@ -0,0 +1,4 @@
+#WAP to enter name and age, then display it.
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+print("Your name is",name,"and your age is",age,"years old.")
--- Programs/sum.py
+++ Programs/sum.py
@@ -3,4 +3,4 @@ a=int(input("Enter the first number:"))
 b=int(input("Enter the second number:"))
 c=int(input("Enter the third number:"))
 sum=a+b+c
-print("The sum of the input three numbers is:",sum)
+print('The sum of the input three numbers is:', sum)
