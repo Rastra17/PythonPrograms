14454c81cd86d1bf637f6bffd4db760a0aea199e
rastr <rastradhoj@gmail.com>
2021-02-24 11:24:10 +0545

    Initial commit

---
+++ Programs/areaoftriangle.py
@@ -0,0 +1,5 @@
+#WAP a program to calculate area of a triangle.
+base=int(input("Enter the base:"))
+height=int(input("Enter the height:"))
+area=0.5*base*height
+print("The area of the triangle is:",area)
---
+++ Programs/sum.py
@@ -0,0 +1,6 @@
+#WAP a program to take sum of three numbers and print it.
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+c=int(input("Enter the third number:"))
+sum=a+b+c
+print("The sum of the input three numbers is:",sum)
