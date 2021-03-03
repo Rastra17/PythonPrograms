e3802694c45cbe01c5c4181e93849af42c2243fd
rastr <rastradhoj@gmail.com>
2021-03-03 12:18:05 +0545

    All files

---
+++ Programs/bmi.py
@@ -0,0 +1,5 @@
+#WAP to calculate BMI
+mass=int(input("Enter your mass in kilograms:"))
+height=int(input("Enter your height in meters:"))
+BMI=[mass/(height**2)]
+print("Your BMI is",BMI)
---
+++ Programs/waittime.py
@@ -0,0 +1,6 @@
+#WAP to calculate the wait time of the bus. You live 4 miles from the university.
+#The bus drives at 25mph but stops 2 minutes at each 10 stops on the way.
+#How long will the bus journey take?
+#Alternatively, you could run to the university and your speed is 7mph for the first mile.
+#The next 2 miles at 15mph before jogging the last at 7mph.
+#Will the bus be quicker or slower than you?
