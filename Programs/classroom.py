d7d57362888ca3766901d492baf237daab21788e
rastr <rastradhoj@gmail.com>
2021-03-07 16:10:43 +0545

    All files

--- Programs/classroom.py
+++ Programs/classroom.py
@@ -1,16 +1,32 @@
 #WAP to calculate the distribution of desks in the least amount in 3 classrooms where 2 students can occupy a desk.
 def clsA(A):
-    least=(A//2)
-    print("The least number of desks for classroom A is:",least)
-    return least
+    if(A%2==0):
+        least = (A // 2)
+        print("The least number of desks for classroom A is:", least)
+        return least
+    else:
+        least = (A // 2)+1
+        print("The least number of desks for classroom A is:", least)
+        return least
 def clsB(B):
-    least=(B//2)
-    print("The least number of desks for classroom B is:",least)
-    return least
+    if(B%2==0):
+        least = (B // 2)
+        print("The least number of desks for classroom B is:", least)
+        return least
+
+    else:
+        least = (B // 2)+1
+        print("The least number of desks for classroom B is:", least)
+        return least
 def clsC(C):
-    least=(C//2)
-    print("The least number of desks for classroom C is:",least)
-    return least
+    if(C%2==0):
+        least = (C // 2)
+        print("The least number of desks for classroom C is:", least)
+        return least
+    else:
+        least = (C // 2)+1
+        print("The least number of desks for classroom C is:", least)
+        return least
 a=int(input("Enter the number of students in classroom A:"))
 b=int(input("Enter the number of students in classroom B:"))
 c=int(input("Enter the number of students in classroom C:"))
