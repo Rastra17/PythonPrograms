43b35cd0fe13fef006d80475f1059b8a0d1656dd
rastr <rastradhoj@gmail.com>
2021-03-10 11:27:31 +0545

    All files

---
+++ Programs/circlearena.py
@@ -0,0 +1,4 @@
+radius=int(input("Enter the radius:"))
+pi=3.1415
+area=pi*(radius**2)
+print("The area of the circle is:",area)
--- Programs/waittime.py
+++ Programs/waittime.py
@@ -4,10 +4,14 @@
 #Alternatively, you could run to the university and your speed is 7mph for the first mile.
 #The next 2 miles at 15mph before jogging the last at 7mph.
 #Will the bus be quicker or slower than you?
-bspeed=25
-wait=0
+speed=25
 intervals=2*10
 distance=4
 startstop=7
 mid=15
-busreach=bspeed-intervals
+timebybus=(distance/speed)+intervals
+timebyus=distance/(startstop+mid+startstop)
+if(timebybus>timebyus):
+    print("The bus is quicker to reach the destination taking",timebybus,"minutes.")
+else:
+    print("The bus is slower to reach the destination",timebyus,"minutes.")
