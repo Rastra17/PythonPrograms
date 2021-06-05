1171ba60a3ebad030db409a9f85a383bc37ca63d
Rastra17 <57227732+Rastra17@users.noreply.github.com>
2021-06-05 22:38:36 +0545

    Complex Programming

--- Programs/classTk.py
+++ Programs/classTk.py
@@ -7,11 +7,11 @@ class Geto:
     def __init__(self, root):
         self.root = root
         self.root.geometry("400x400")
-        self.entry_Label = tk.Label(self.root, text="Input:")
+        self.entry_Label = tk.Label(self.root, text="Enter name:")
         self.entry_Label.pack()
         self.entry_Entry = tk.Entry(self.root, borderwidth=5)
         self.entry_Entry.pack()
-        self.entry_Button = tk.Button(self.root, text="Enter", command=self.get)
+        self.entry_Button = tk.Button(self.root, text="Confirm", command=self.get)
         self.entry_Button.pack()
         self.entry_Button1 = tk.Button(
             self.root, text="Quit", command=self.quit
