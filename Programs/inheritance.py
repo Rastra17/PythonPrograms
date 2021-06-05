542ff4d8f966614edd5b0aba549980a7ce885566
rastr <rastradhoj@gmail.com>
2021-06-05 19:34:24 +0545

    Complex Programming

---
+++ Programs/inheritance.py
@@ -0,0 +1,17 @@
+class Coder:
+    def __init__(self,ethical,networking,computing):
+        self.ethical=ethical
+        self.networking=networking
+        self.computing=computing
+
+class Person(Coder):
+    def test(self):
+        pass
+
+class Program(Person):
+    def behaviour(self):
+        pr = self.ethical + self.networking + self.computing
+        print(pr)
+
+person1=Program("1","2","3")
+person1.behaviour()
