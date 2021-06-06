72d7b00923234f1d00afefa98df6db95f1033b2c
Rastra17 <57227732+Rastra17@users.noreply.github.com>
2021-06-06 10:56:34 +0545

    Complex Programming

--- Programs/classTk.py
+++ Programs/classTk.py
@@ -24,7 +24,7 @@ class Geto:
         self.Display = tk.Label(self.root, text=self.data)
         self.Display.pack()
         miracle.append(self.data)
-        return self.data[-1]
+        return self.data
 
     def quit(self):
         self.root.destroy()
