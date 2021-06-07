314a40e3b0b0d1ceab082cce19c2501f98e28def
Rastra17 <57227732+Rastra17@users.noreply.github.com>
2021-06-07 14:35:58 +0545

    Complex Programming

---
+++ Programs/argskwargs.py
@@ -0,0 +1,28 @@
+#A normal no return, yes argument function
+def function_name_print(a,b,c,d):
+    print(a,b,c,d)
+print("This is a normal function:")
+function_name_print("Harry","Alex","Bell","Red\n")
+
+#Declaring *args function which takes multiple input within a function with correct position of arguments.
+def funargs(strin,*args):
+    lim=len(args)
+    print(strin)
+    for i in range(lim):
+        print(args[i])
+strin="This is an *args function:"
+lst=["Harry","Alex","Bell","Red","Rona\n"]
+funargs(strin,*lst)
+
+#Declaring **kwargs function which takes in values of a dictionary within a function with correct position of arguments.
+def funkwargs(strin,*args,**kwargs):
+    lim=len(args)
+    print(strin)
+    for i in range(lim):
+        print(args[i])
+    for key,value in kwargs.items():
+        print(f"{key} is a {value}")
+strin="This is a **kwargs function:"
+args=["Harry","Alex","Bell","Red","Rona\n"]
+kwargs={"Alex":"Monitor","Alexandra":"Student Council President","Alexsaur":"College Captain"}
+funkwargs(strin,*args,**kwargs)
